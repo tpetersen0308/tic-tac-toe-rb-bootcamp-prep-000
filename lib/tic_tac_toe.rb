7f7b3aaa9d8563376c37d2840cc2376bef7ccd0e
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-09 19:29:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,15 +4,15 @@ def position_taken?(board, index)
 end
 
 def display_board(board)
-  puts " #{board[]} | #{board[]} | #{board[]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " #{board[]} | #{board[]} | #{board[]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[]} | #{board[]} | #{board[]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index()
-  index = gets.strip.to_i - 1
+  gets.strip.to_i - 1
 end
 
 
