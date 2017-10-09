cf511e4a0ab54fa0395c28ecd8c2fc273e29409f
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-09 19:36:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ def move(board, index, current_player)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 
